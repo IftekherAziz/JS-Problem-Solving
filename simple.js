a981e001115396b1985fcf272339397ade44b270
Iftekher Aziz <azizchy02@gmail.com>
2023-02-08 04:03:24 +0600

    factorial js

--- simple.js
+++ simple.js
@@ -4,8 +4,32 @@
 // }
 // console.log(sum);
 
-let sum = 0;
-for (let i =5; i >= 1; i--) {
-    sum += i;
+// let sum = 0;
+// for (let i =5; i >= 1; i--) {
+//     sum += i;
+// }
+// console.log(sum);
+/**
+ * "The function sum() takes in a number, and returns the sum of all numbers from that number down to
+ * 0."
+ * 
+ * The function sum() takes in a number, and returns the sum of all numbers from that number down to 0
+ * @param i - 5
+ */
+
+function sum(i) {
+  if (i == 1){
+    return 1;
+  }
+  console.log(i);
+  return i + sum(i - 1);
 }
-console.log(sum);
+console.log(sum(5));
+
+/* 
+   5 + sum(4)
+   5 + 4 + sum(3)
+   5 + 4 + 3 + sum(2)
+   5 + 4 + 3 + 2 + sum(1)
+   5 + 4 + 3 + 2 + 1
+*/
