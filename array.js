934c08b0063e24763f1536c951caf7adc06a64d1
Iftekher Aziz <azizchy02@gmail.com>
2023-02-08 04:20:10 +0600

    update code

---
+++ array.js
@@ -0,0 +1,15 @@
+const products = [ 
+    { name: 'cucumber', type: 'vegetable' },
+    { name: 'banana', type: 'fruit' },
+    { name: 'celery', type: 'vegetable' },
+    { name: 'orange', type: 'fruit' }
+];
+// products[0];
+// products[1];
+
+// const products = [ 15, 56, 87 ];
+// const products = {
+//     '0': 15,
+//     '1': 56,
+//     '2': 87
+// }
