4da99400c516f89ad32e1cb902ecc5b8ef67fb8d
Iftekher Aziz <azizchy02@gmail.com>
2023-02-07 22:57:33 +0600

    Upload

--- simple.js
+++ simple.js
@@ -1,5 +1,11 @@
+// let sum = 0;
+// for (var i = 1; i <=5; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
 let sum = 0;
-for (var i = 1; i <=5; i++) {
+for (let i =5; i >= 1; i--) {
     sum += i;
 }
 console.log(sum);
